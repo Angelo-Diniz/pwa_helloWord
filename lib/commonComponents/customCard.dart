8fe0c7770faf60963a070942306c1a5c421281cf
Breno Mendes <breno.hfm@gmail.com>
2020-03-31 21:41:49 -0300

    Iniciado projeto gym_city

---
+++ lib/commonComponents/customCard.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../gym_detail.dart';
+
+class CustomCard extends StatelessWidget {
+  CustomCard({@required this.title, this.description});
+
+  final title;
+  final description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        padding: const EdgeInsets.only(top: 5.0),
+        child: Column(
+          children: <Widget>[
+            Text(title),
+            FlatButton(
+                child: Text("Clique e veja mais"),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => GymDetail(
+                              title: title, description: description)));
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/login.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:gym_city/home.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+  TextEditingController emailInputController;
+  TextEditingController pwdInputController;
+
+  @override
+  initState() {
+    emailInputController = new TextEditingController();
+    pwdInputController = new TextEditingController();
+    super.initState();
+  }
+
+  String emailValidator(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value)) {
+      return 'Formato de email inválido';
+    } else {
+      return null;
+    }
+  }
+
+  String pwdValidator(String value) {
+    if (value.length < 8) {
+      return 'A sua senha deve ter mais que 8 caracteres';
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Login"),
+        ),
+        body: Container(
+            padding: const EdgeInsets.all(20.0),
+            child: SingleChildScrollView(
+                child: Form(
+              key: _loginFormKey,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 50.0),
+                  Container(
+                    width: 200,
+                    height: 200,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/logo.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Email*', hintText: "joao.silva@gmail.com"),
+                    controller: emailInputController,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: emailValidator,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: 'Senha*', hintText: "********"),
+                    controller: pwdInputController,
+                    obscureText: true,
+                    validator: pwdValidator,
+                  ),
+                  RaisedButton(
+                    child: Text("Login"),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      if (_loginFormKey.currentState.validate()) {
+                        FirebaseAuth.instance
+                            .signInWithEmailAndPassword(
+                                email: emailInputController.text,
+                                password: pwdInputController.text)
+                            .then((currentUser) => Firestore.instance
+                                .collection("users")
+                                .document(currentUser.user.uid)
+                                .get()
+                                .then((DocumentSnapshot result) =>
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => HomePage(
+                                                  title: result["fname"] +
+                                                      "- Suas avaliações",
+                                                  uid: currentUser.user.uid,
+                                                ))))
+                                .catchError((err) => print(err)))
+                            .catchError((err) => print(err));
+                      }
+                    },
+                  ),
+                  Text("Ainda não tem conta no nosso App? "),
+                  FlatButton(
+                    child: Text("Cadastra-se aqui!"),
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/register");
+                    },
+                  )
+                ],
+              ),
+            ))));
+  }
+}
